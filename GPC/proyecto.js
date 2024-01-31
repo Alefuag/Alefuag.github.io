4620383863bb401573a9e3c86ba685c21d836d44
alejandro <juafuag@vrain.upv.es>
2024-01-31 02:57:37 +0100

    Refactor code to improve performance and readability

--- GPC/proyecto.js
+++ GPC/proyecto.js
@@ -7,14 +7,14 @@ import { TWEEN } from "./lib/tween.module.min.js"
 import { GUI } from "./lib/lil-gui.module.min.js";
 // import { stats } from "../lib/stats.module.js";
 
+const sleep = ms => new Promise(r => setTimeout(r, ms));
 
 //variables estandar
 let renderer, scene, camera, top_camera, controls;
 
-
-let floor_material, material;
+let playerMaterial, floor_material, material;
 let clock = new THREE.Clock();
-let speed = 200, num_obstaculos = 20;
+let speed = 500, num_obstaculos = 200, distancia_entre_obstaculos = 1000;
 let right_arrow = false, left_arrow = false, up_arrow = false, down_arrow = false;
 let AmbientLight, directionalLight, pointLight, spotLight;
 let player, playerBody, obstacles = [], obstacleBodies = [], world;
@@ -26,21 +26,25 @@ initCannon();
 initLighting();
 initGUI();
 render();
-clock.stop();
+// clock.stop();
 
 
 function checkDown(e) {
     if (e.keyCode == '38') { up_arrow = true; }
-    else if (e.keyCode == '40') { down_arrow = true; }
-    else if (e.keyCode == '37') { left_arrow = true; }
-    else if (e.keyCode == '39') { right_arrow = true; }
+    else if (e.keyCode == '40') { down_arrow = true;}
+    else if (e.keyCode == '37') { left_arrow = true; playerBody.velocity.x = speed;}
+    else if (e.keyCode == '39') { right_arrow = true; playerBody.velocity.x = -speed;}
+    else if (e.keyCode == '80') { clock.stop(); }
+    else if (e.keyCode == '32') { clock.start(); }
+    else if (e.keyCode == '83') { startGame(); }
+    // console.log(e.keyCode);
 }
 
 function checkUp(e) {
     if (e.keyCode == '38') { up_arrow = false; }
     else if (e.keyCode == '40') { down_arrow = false; }
-    else if (e.keyCode == '37') { left_arrow = false; }
-    else if (e.keyCode == '39') { right_arrow = false; }
+    else if (e.keyCode == '37') { left_arrow = false; playerBody.velocity.x = 0;}
+    else if (e.keyCode == '39') { right_arrow = false; playerBody.velocity.x = 0;}
 }
 
 
@@ -63,13 +67,13 @@ function init() {
     scene = new THREE.Scene()
     scene.background = new THREE.Color( 0x220044 )
 
-    camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 1, 10000)
-    camera.position.set(0, 200, -200);
-    camera.lookAt(0, 60, 0);
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 1, 100000)
+    camera.position.set(0, 20, -20);
+    camera.lookAt(0, 6, 0);
     // scene.add(camera);
 
-    top_camera = new THREE.OrthographicCamera(-100, 100, 100, -100, 1, 1000)
-    top_camera.position.set(0, 300, 0)
+    top_camera = new THREE.OrthographicCamera(-10, 10, 10, -10, 0.1, 1000)
+    top_camera.position.set(0, 30, 0)
     top_camera.lookAt(0, 0, 0)
     // scene.add(top_camera)
 
@@ -87,17 +91,41 @@ function rad_to_deg(radians) {
 
 function loadScene() {
     material = new THREE.MeshPhongMaterial({color:'white', wireframe:false})
+    floor_material = new THREE.MeshPhongMaterial({color:'white', wireframe:false})
+    playerMaterial = new THREE.MeshPhongMaterial({color:'white', wireframe:false})
     
+    // add floor
     const suelo = new THREE.Mesh(new THREE.PlaneGeometry(1000, 100000, 500, 500), material)
     suelo.rotation.x = -Math.PI/2;
     suelo.position.y = -0.2;
-    suelo.receiveShadow = true;
-    
+    suelo.receiveShadow = true; 
+
     scene.add(suelo)
     
-    // add image texture to the ground
+    // add walls
+    const boundaryGeometry = new THREE.BoxGeometry(10, 50, 100000);
+    const boundaryMaterial = new THREE.MeshBasicMaterial({color: 'brown', wireframe: false});
+    const boundary1 = new THREE.Mesh(boundaryGeometry, boundaryMaterial);
+    const boundary2 = new THREE.Mesh(boundaryGeometry, boundaryMaterial);
+
+    boundary1.position.set(-500, 25, 0);
+    boundary2.position.set(500, 25, 0);
+
+    scene.add(boundary1);
+    scene.add(boundary2);
+    
+    
     var loader = new THREE.TextureLoader();
     var basepath = './images/';
+    loader.setPath( basepath );
+    var playertexture = loader.load('ball.jpg');
+    playertexture.wrapS = THREE.RepeatWrapping;
+    playertexture.wrapT = THREE.RepeatWrapping;
+    playertexture.repeat.set( 1, 1 );
+    playerMaterial.map = playertexture;
+
+    // add image texture to the ground
+    var loader = new THREE.TextureLoader();
     loader.setPath( basepath );
     var texture = loader.load('brick_wall.jpg');
     texture.wrapS = THREE.RepeatWrapping;
@@ -119,22 +147,25 @@ function loadScene() {
     //add background texture
     scene.background = textureCube;
 
-    // add player
-    player = new THREE.Object3D();
-    var player_loader = new GLTFLoader();
-    var basepath = './models/robota/';
-    player_loader.setPath(basepath);
-    player_loader.load('scene.gltf', function (gltf) {
-        player.add(gltf.scene);
-        player.scale.set(0.5, 0.5, 0.5);
-        player.position.set(0, 50, 0);
-        player.rotation.y = Math.PI;
-        player.name = "player";
-        player.castShadow = true;
-        player.receiveShadow = true;
-        scene.add(player);
-        console.log('robot loaded');
-    });
+    // add player as sphere
+    player = new THREE.Mesh(new THREE.SphereGeometry(25, 32, 32), playerMaterial)
+    player.position.set(0, 50, 0);
+    player.castShadow = true;
+    player.receiveShadow = true;
+    scene.add(player)
+
+    // Generate random obstacles
+    for (let i = 0; i < num_obstaculos; i++) {
+        const obstacle = new THREE.Mesh(new THREE.BoxGeometry(100 + 250*Math.random(), 50, 50), material);
+        const x = Math.random() * 800 - 400; // Random x position between -450 and 450
+        const y = 25; // Fixed y position
+        const z = distancia_entre_obstaculos*(i+1);
+        obstacle.position.set(x, y, z);
+        obstacle.castShadow = true;
+        obstacle.receiveShadow = true;
+        scene.add(obstacle);
+        obstacles.push(obstacle);
+    }
 }
 
 function initLighting() {
@@ -187,73 +218,107 @@ function initLighting() {
 function initGUI() {
     const gui = new GUI()
     // create degrees variable for each joint
-    var playpause = {play: function() {clock.start()}, pause: function() {clock.stop()}}
+    var playpause = {play: function() {clock.start(); }, pause: function() {clock.stop()}}
 
-    gui.add(playpause, 'play').name('Play')
-    gui.add(playpause, 'pause').name('Pause')
-    gui.add({restart: function() {restartGame()}}, 'restart').name('Restart')
+    gui.add({start: startGame}, 'start').name('Start Game (S)')
+    gui.add(playpause, 'play').name('Play (Space)')
+    gui.add(playpause, 'pause').name('Pause (P)')
 }
 
+function startGame() {
+    clock.start();
+    playerBody.velocity.set(0, 0, 1000);
+}
 
 function initCannon() {
     world = new CANNON.World();
-    world.gravity.set(0, 0, 0);
-    world.broadphase = new CANNON.NaiveBroadphase();
-    world.solver.iterations = 10;
+    world.gravity.set(0, -9.81, 0);
+    const groundCannonMaterial = new CANNON.Material("groundMaterial");
+    const playerCannonMaterial = new CANNON.Material("playerMaterial");
+
+    const ground_player_cm = new CANNON.ContactMaterial(groundCannonMaterial, playerCannonMaterial, {
+        friction: 0.0,
+        restitution: 0.0,
+        contactEquationStiffness: 1e8,
+        contactEquationRelaxation: 3,
+        frictionEquationStiffness: 1e8,
+        frictionEquationRegularizationTime: 3,
+    });
+    world.addContactMaterial(ground_player_cm);
 
     // Create ground
-    var groundShape = new CANNON.Plane();
-    var groundBody = new CANNON.Body({ mass: 0 });
-    groundBody.addShape(groundShape);
+    var groundBody = new CANNON.Body({
+        shape: new CANNON.Plane(),
+        type: CANNON.Body.STATIC,
+        material: groundCannonMaterial,
+    });
 
     groundBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2);
     world.addBody(groundBody);
 
     // Create player collider
-    var playerShape = new CANNON.Box(new CANNON.Vec3(50, 50, 10));
-    playerBody = new CANNON.Body({ mass: 1 });
-    playerBody.addShape(playerShape);
-    playerBody.position.set(0, 50, 0);  
-    playerBody.velocity.set(0, 0, 1000);
+    // shape: new CANNON.Sphere(25), // new CANNON.Box(new CANNON.Vec3(10, 50, 10) ), // new CANNON.Sphere(30),
+    playerBody = new CANNON.Body({
+        position: new CANNON.Vec3(0, 50, 0),
+        mass: 1,
+        material: playerCannonMaterial,
+    });
+    playerBody.addShape(new CANNON.Sphere(25));
+    playerBody.linearDamping = 0;
+    playerBody.velocity.set(0, 0, 0);
     world.addBody(playerBody);
 
-    // Create obstacles colliders
+    // add wall colliders
+    var boundaryBody1 = new CANNON.Body({
+        shape: new CANNON.Box(new CANNON.Vec3(10, 10, 100000)),
+        type: CANNON.Body.STATIC,
+        material: groundCannonMaterial,
+    });
+    boundaryBody1.position.set(-500, 50, 0);
+    world.addBody(boundaryBody1);
+
+    var boundaryBody2 = new CANNON.Body({
+        shape: new CANNON.Box(new CANNON.Vec3(10, 10, 100000)),
+        type: CANNON.Body.STATIC,
+        material: groundCannonMaterial,
+    });
+    boundaryBody2.position.set(500, 50, 0);
+    world.addBody(boundaryBody2);
+
+    // add obstacle colliders
     for (let i = 0; i < obstacles.length; i++) {
-        var obstacleShape = new CANNON.Box(new CANNON.Vec3(50, 50, 10));
-        var obstacleBody = new CANNON.Body({ mass: 0 });
-        obstacleBody.addShape(obstacleShape);
+        const x = obstacles[i].position.x;
+        const y = obstacles[i].position.y;
+        const z = obstacles[i].position.z;
+        
+        const height = obstacles[i].geometry.parameters.height;
+        const width = obstacles[i].geometry.parameters.width;
+        const depth = obstacles[i].geometry.parameters.depth;
 
-        // Randomly position obstacles in front of the player
-        var randomX = Math.random() * 200 - 100; // Random X position between -100 and 100
-        var randomZ = -Math.random() * 200; // Random Z position behind the player
-        obstacleBody.position.set(randomX, 50, randomZ);
-
+        const obstacleBody = new CANNON.Body({
+            shape: new CANNON.Box(new CANNON.Vec3(height/2, width/2, depth/2)),
+            position: new CANNON.Vec3(x, y, z),
+            type: CANNON.Body.STATIC,
+            material: groundCannonMaterial,
+            isTrigger: true,
+        });
+        
+        obstacleBody.addEventListener("collide", function (e) {
+            if (e.body === playerBody) {
+                console.log("Player collided with obstacle");
+                playerBody.velocity.z = 0;
+                playerBody.velocity.x = 0;
+                playerBody.velocity.y = 0;
+                playerBody.position.set(0, 50, 0);
+                playerBody.quaternion.set(0, 0, 0, 1);
+            }
+        });
         world.addBody(obstacleBody);
-    }
+        obstacleBodies.push(obstacleBody);
+    } 
 }
 
 
-function checkCollision() {
-    for (let i = 0; i < obstacleBodies.length; i++) {
-        let distance = playerBody.position.distanceTo(obstacleBodies[i].position);
-        if (distance < player.geometry.boundingSphere.radius + obstacles[i].geometry.boundingSphere.radius) {
-            restartGame();
-            break;
-        }
-    }
-}
-
-function restartGame() {
-    // Reset player position and velocity
-    playerBody.position.set(0, 0, 0);
-    playerBody.velocity.set(0, 0, 0);
-
-    // Reset obstacle positions
-    for (let i = 0; i < obstacleBodies.length; i++) {
-        obstacleBodies[i].position.set(0, 0, 0);
-        obstacleBodies[i].velocity.set(0, 0, 0);
-    }
-}
 
 function update() {
     let delta = clock.getDelta();
@@ -267,13 +332,15 @@ function update() {
         // jump(delta);
     }
     if (down_arrow) {
-
+        dont_move(delta);
     }
 
     // update physics
     if (delta > 0) {
         world.step(delta);
     }
+
+    // update player (sphere)
     player.position.copy(playerBody.position);
     player.quaternion.copy(playerBody.quaternion);
 
@@ -285,16 +352,12 @@ function update() {
     camera.lookAt(new THREE.Vector3(player.position.x, player.position.y + 50, player.position.z));
 
     // update obstacles
-    for (let i = 0; i < obstacles.length; i++) {
-        obstacles[i].position.copy(obstacles[i].body.position);
-        obstacles[i].quaternion.copy(obstacles[i].body.quaternion);
-    }
+    // for (let i = 0; i < obstacles.length; i++) {
+    //     obstacles[i].position.copy(obstacles[i].body.position);
+    //     obstacles[i].quaternion.copy(obstacles[i].body.quaternion);
+    // }
 
-    // check collision
-    checkCollision();
 }
-
-
 
 function move_right(delta) {
     playerBody.velocity.x = -speed;
@@ -306,6 +369,10 @@ function jump(delta) {
 
 function move_left(delta) {
     playerBody.velocity.x = speed;
+}
+
+function dont_move(delta) {
+    playerBody.velocity.x = 0;
 }
 
 
